9371778bb0fbc63dc1da8ec709c6864843e42e10
SegeiShelest <sergei_ragulev@mail.ru>
2018-02-23 15:37:34 +0300

    Add settings and start page

--- lua/DermaChanger/cl_gui.lua
+++ lua/DermaChanger/cl_gui.lua
@@ -2,7 +2,6 @@ local PANEL = {}
 
 local function Menu()
 	local function LoadTheme()
-
 		PANEL.Open = vgui.Create("DFrame")
 		PANEL.Open:SetSize(600, 650)
 		PANEL.Open:SetSizable(false)
@@ -14,18 +13,17 @@ local function Menu()
 		PANEL.Open.Browser:SetPos(0, 25)
 		PANEL.Open.Browser:SetSize(600, 650)
 		PANEL.Open.Browser:SetPath("GAME")
-		PANEL.Open.Browser:SetBaseFolder("data/DermaChanger/themes")
+		PANEL.Open.Browser:SetBaseFolder("data/dermachanger/themes")
 		PANEL.Open.Browser:SetOpen(true)
 		PANEL.Open.Browser:SetCurrentFolder("persist")
 		
 		function PANEL.Open.Browser:OnSelect(path, pnl)
 			local f = file.Read(path, "GAME")
-			d = util.JSONToTable(f)
+			DCLoadTheme(d, util.JSONToTable(f))
 		end
 	end
 
 	local function SaveTheme()
-
 		PANEL.Save = vgui.Create("DFrame")
 		PANEL.Save:SetSize(400, 55)
 		PANEL.Save:SetSizable(false)
@@ -42,13 +40,31 @@ local function Menu()
 		PANEL.Save.Button:SetSize(35, 25)
 		PANEL.Save.Button:SetText("Save")
 		PANEL.Save.Button.DoClick = function()
-
 			local tab = util.TableToJSON(d)
 			local name = PANEL.Save.TextEntry:GetValue()
-			file.CreateDir("DermaChanger/themes")
-			file.Write("DermaChanger/themes/"..name..".txt", tab)
+			
+			file.CreateDir("dermachanger/themes")
+			file.Write("dermachanger/themes/"..name..".txt", tab)
 
-			PANEL.Save = nil
+			PANEL.Save:Remove()
+		end
+	end
+
+	local function Settings()
+		SetAutoLoad = vgui.Create("DButton", PANEL.Panel)
+		SetAutoLoad:SetPos(30, 20)
+		SetAutoLoad:SetSize(150, 28)
+		SetAutoLoad:SetText("Set as startup")
+		SetAutoLoad.DoClick = function()
+			file.Write("dermachanger/al.txt", util.TableToJSON(d))
+		end
+
+		SetAutoLoad = vgui.Create("DButton", PANEL.Panel)
+		SetAutoLoad:SetPos(30, 54)
+		SetAutoLoad:SetSize(150, 28)
+		SetAutoLoad:SetText("Remove startup")
+		SetAutoLoad.DoClick = function()
+			file.Write("dermachanger/al.txt", "{}")
 		end
 	end
 
@@ -67,7 +83,6 @@ local function Menu()
 		button:SetText("")
 		
 		button.DoClick = function()
-			
 			local Frame = vgui.Create("DFrame")
 			Frame:SetSize(265, 300)
 			Frame:SetTitle(Label)
@@ -95,7 +110,6 @@ local function Menu()
 		button.Paint = function(self, w, h)
 			draw.RoundedBox(10, 0, 0, w, h, col)
 		end
-
 	end
 
 	local function SettingInt(x, y, Label, value, value2)
@@ -111,9 +125,7 @@ local function Menu()
 		button:SetPos(x, y)
 		button:SetSize(20, 20)
 		button:SetText(int)
-		
 		button.DoClick = function()
-		
 			local Frame = vgui.Create("DFrame")
 			Frame:SetSize(265, 200)
 			Frame:SetTitle(Label)
@@ -159,9 +171,7 @@ local function Menu()
 		button:SetPos(x, y)
 		button:SetSize(20, 20)
 		button:SetText("STR")
-		
 		button.DoClick = function()
-
 			local Frame = vgui.Create("DFrame")
 			Frame:SetSize(400, 55)
 			Frame:SetSizable(false)
@@ -193,6 +203,47 @@ local function Menu()
 		label:SetPos(x, y)
 		label:SizeToContents()
 		label:SetText(text)
+	end
+
+	local function welcomePage()
+		local label = vgui.Create("DLabel", PANEL.Panel)
+		label:SetText("Welcome")
+		label:SetFont("DermaLarge")
+		label:SetPos(30, 30)
+		label:SizeToContents()
+
+		local label1 = vgui.Create("DLabel", PANEL.Panel)
+		label1:SetText("Author:")
+		label1:SetFont("DermaDefaultBold")
+		label1:SetPos(40, 70)
+		label1:SizeToContents()
+
+		local label2 = vgui.Create("DLabelURL", PANEL.Panel)
+		label2:SetText("Shelest_ (Steam)(steamcommunity.com/profiles/76561198171200633)")
+		label2:SetURL("http://steamcommunity.com/profiles/76561198171200633")
+		label2:SetColor(Color(0, 119, 255))
+		label2:SetPos(40, 85)
+		label2:SizeToContents()
+
+		local label3 = vgui.Create("DLabelURL", PANEL.Panel)
+		label3:SetText("SergeiShelest (GitHub)(github.com/SergeiShelest)")
+		label3:SetURL("https://github.com/SergeiShelest")
+		label3:SetColor(Color(0, 119, 255))
+		label3:SetPos(40, 100)
+		label3:SizeToContents()
+
+		local label4 = vgui.Create("DLabel", PANEL.Panel)
+		label4:SetText("Source:")
+		label4:SetFont("DermaDefaultBold")
+		label4:SetPos(40, 140)
+		label4:SizeToContents()
+
+		local label5 = vgui.Create("DLabelURL", PANEL.Panel)
+		label5:SetText("GitHub (github.com/SergeiShelest/DermaChanger)")
+		label5:SetURL("https://github.com/SergeiShelest/DermaChanger")
+		label5:SetColor(Color(0, 119, 255))
+		label5:SetPos(40, 155)
+		label5:SizeToContents()
 	end
 
 	PANEL.Base = vgui.Create("DFrame")
@@ -231,6 +282,12 @@ local function Menu()
 	PANEL.Settings:SetPos(5, 465 +15+30+28+28)
 	PANEL.Settings:SetSize(150, 28)
 	PANEL.Settings:SetText("Settings")
+	PANEL.Settings.DoClick = function()
+		PANEL.Panel:Clear()
+		Settings()
+	end
+
+	welcomePage()
 
 	local components = table.GetKeys(d)
 
@@ -245,16 +302,15 @@ local function Menu()
 			PANEL.Panel:Clear()
 
 			for key, settting in pairs(d[component]) do
-
-				if TypeID(settting) == 3 then
+				if TypeID(settting) == TYPE_NUMBER then
 					SettingInt(10 + py, px, t[component][key], component, key)
 				end
 
-				if TypeID(settting) == 4 then
+				if TypeID(settting) == TYPE_STRING then
 					SettingStr(10 + py, px, t[component][key], component, key)
 				end
 
-				if TypeID(settting) == 5 then
+				if TypeID(settting) == TYPE_TABLE then
 					SettingColor(10 + py, px, t[component][key], component, key)
 				end
 
@@ -264,7 +320,6 @@ local function Menu()
 				    py = 0
 				    px = px + 30
 				end
-
 			end
 		end
 	end
--- lua/DermaChanger/cl_init.lua
+++ lua/DermaChanger/cl_init.lua
@@ -9,5 +9,29 @@ for _, component in pairs(components) do
 	include("DermaChanger/components/"..component)
 end
 
+function DCLoadTheme(table1, table2) --Спасибо Коля
+	for k, v in pairs(table1) do
+		for k2, v2 in pairs(table2) do
+			if k == k2 then
+				if TypeID(v2) ~= TYPE_TABLE then
+					table1[k] = v2
+				else
+					DCLoadTheme(table1[k], v2)
+				end
+			end
+		end
+	end
+end
+
+if not file.Exists("data/dermachanger/al.txt", "GAME") then
+	print("[DermaChanger] Creating an autorun file")
+	print("[DermaChanger] Creating data/dermachanger/al.txt")
+	file.CreateDir("dermachanger")
+	file.Write("dermachanger/al.txt", "{}")
+end
+
+local f = file.Read("data/dermachanger/al.txt", "GAME")
+DCLoadTheme(d, util.JSONToTable(f))
+
 include("DermaChanger/cl_gui.lua")
 
