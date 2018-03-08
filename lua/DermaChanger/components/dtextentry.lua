ababe2a462b5568e860de7f022bd385ba1184553
SergeiShelest <sergei_ragulev@mail.ru>
2018-03-08 12:46:27 +0300

    Border

--- lua/DermaChanger/components/dtextentry.lua
+++ lua/DermaChanger/components/dtextentry.lua
@@ -5,7 +5,11 @@ t["TextEntry"]["TITLE"] = "TextEntry"
 d["TextEntry"]["BackgroundDisabled"], t["TextEntry"]["BackgroundDisabled"] = Color(80, 80, 80), "BackgroundDisabled"
 d["TextEntry"]["BackgroundFocus"], t["TextEntry"]["BackgroundFocus"] = Color(80, 80, 80), "BackgroundFocus"
 d["TextEntry"]["Background"], t["TextEntry"]["Background"] = Color(50, 50, 50), "Background"
-d["TextEntry"]["CornerRadius"], t["TextEntry"]["CornerRadius"] = 0, "Corner radius"
+d["TextEntry"]["BorderDisabled"], t["TextEntry"]["BorderDisabled"] = Color(50, 50, 50), "BorderDisabled"
+d["TextEntry"]["BorderFocus"], t["TextEntry"]["BorderFocus"] = Color(197, 122, 27), "BorderFocus"
+d["TextEntry"]["Border"], t["TextEntry"]["Border"] = Color(227, 152, 57), "Border"
+d["TextEntry"]["BorderSize"], t["TextEntry"]["BorderSize"] = 1, "BorderSize"
+d["TextEntry"]["CornerRadius"], t["TextEntry"]["CornerRadius"] = 3, "Corner radius"
 d["TextEntry"]["TitleColor"], t["TextEntry"]["TitleColor"] = Color(100, 100, 100), "Title color"
 d["TextEntry"]["TextColor"], t["TextEntry"]["TextColor"] = Color(255, 255, 255), "Text color"
 d["TextEntry"]["SelectColor"], t["TextEntry"]["SelectColor"] = Color(217, 142, 47), "Select color"
@@ -15,11 +19,14 @@ function SKIN:PaintTextEntry( panel, w, h )
 	if ( panel.m_bBackground ) then
 
 		if ( panel:GetDisabled() ) then
-			draw.RoundedBox(d["TextEntry"]["CornerRadius"], 0, 0, w, h, d["TextEntry"]["BackgroundDisabled"])
+			draw.RoundedBox(d["TextEntry"]["CornerRadius"], 0, 0, w, h, d["TextEntry"]["BorderDisabled"])
+			draw.RoundedBox(d["TextEntry"]["CornerRadius"], d["TextEntry"]["BorderSize"], d["TextEntry"]["BorderSize"], w-1, h-1, d["TextEntry"]["BackgroundDisabled"])
 		elseif ( panel:HasFocus() ) then
-			draw.RoundedBox(d["TextEntry"]["CornerRadius"], 0, 0, w, h, d["TextEntry"]["BackgroundFocus"])
+			draw.RoundedBox(d["TextEntry"]["CornerRadius"], 0, 0, w, h, d["TextEntry"]["BorderFocus"])
+			draw.RoundedBox(d["TextEntry"]["CornerRadius"], d["TextEntry"]["BorderSize"], d["TextEntry"]["BorderSize"], w-d["TextEntry"]["BorderSize"]*2, h-d["TextEntry"]["BorderSize"]*2, d["TextEntry"]["BackgroundFocus"])
 		else
-			draw.RoundedBox(d["TextEntry"]["CornerRadius"], 0, 0, w, h, d["TextEntry"]["Background"])
+			draw.RoundedBox(d["TextEntry"]["CornerRadius"], 0, 0, w, h, d["TextEntry"]["Border"])
+			draw.RoundedBox(d["TextEntry"]["CornerRadius"], d["TextEntry"]["BorderSize"], d["TextEntry"]["BorderSize"], w-d["TextEntry"]["BorderSize"]*2, h-d["TextEntry"]["BorderSize"]*2, d["TextEntry"]["Background"])
 		end
 
 	end
