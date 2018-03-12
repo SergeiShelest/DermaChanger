1d7cb108fc26af56e56cd7390724053493fd2cc7
SergeiShelest <sergei_ragulev@mail.ru>
2018-03-12 10:36:08 +0300

    Fix GovnoKod

--- lua/DermaChanger/components/dcheckbox.lua
+++ lua/DermaChanger/components/dcheckbox.lua
@@ -1,40 +1,64 @@
-d["СheckBox"] = {}
-t["СheckBox"] = {}
+d["СheckBox"], t["СheckBox"] = {}, {}
 
 t["СheckBox"]["TITLE"] = "СheckBox"
 
-d["СheckBox"]["NotActive"] = Color(60, 60, 60)
-d["СheckBox"]["Hovered"] = Color(187, 102, 17)
-d["СheckBox"]["Active"] = Color(197, 122, 27)
-d["СheckBox"]["DisabledActive"] = Color(50, 50, 50)
-d["СheckBox"]["Disabled"] = Color(100, 100, 100)
-d["СheckBox"]["CornerRadius"] = 0
+d["СheckBox"]["NotActive"], t["СheckBox"]["NotActive"] = Color(60, 60, 60), "NotActive"
+d["СheckBox"]["Hovered"], t["СheckBox"]["Hovered"] = Color(187, 102, 17), "Hovered"
+d["СheckBox"]["Active"], t["СheckBox"]["Active"] = Color(197, 122, 27), "Active"
+d["СheckBox"]["DisabledActive"], t["СheckBox"]["DisabledActive"] = Color(50, 50, 50), "DisabledActive"
+d["СheckBox"]["Disabled"], t["СheckBox"]["Disabled"] = Color(100, 100, 100), "Disabled"
 
-t["СheckBox"]["NotActive"] = "Not active"
-t["СheckBox"]["Hovered"] = "Hovered"
-t["СheckBox"]["Active"] = "Active"
-t["СheckBox"]["DisabledActive"] = "Disabled active"
-t["СheckBox"]["Disabled"] = "Disabled"
-t["СheckBox"]["CornerRadius"] = "Corner radius"
+d["СheckBox"]["BorderNotActive"], t["СheckBox"]["BorderNotActive"] = Color(197, 122, 27), "BorderNotActive"
+d["СheckBox"]["BorderHovered"], t["СheckBox"]["BorderHovered"] = Color(187, 102, 17), "BorderHovered"
+d["СheckBox"]["BorderActive"], t["СheckBox"]["BorderActive"] = Color(197, 122, 27), "BorderActive"
+d["СheckBox"]["BorderDisabledActive"], t["СheckBox"]["BorderDisabledActive"] = Color(50, 50, 50), "BorderDisabledActive"
+d["СheckBox"]["BorderDisabled"], t["СheckBox"]["BorderDisabled"] = Color(100, 100, 100), "BorderDisabled"
+
+d["СheckBox"]["BorderSize"], t["СheckBox"]["BorderSize"] = 1, "BorderSize"
+
+d["СheckBox"]["CornerRadius"], t["СheckBox"]["CornerRadius"] = 0, "CornerRadius"
+
+local Col = nil
+local ColBord = nil
 
 function SKIN:PaintCheckBox( panel, w, h )
 
-	if ( panel:GetChecked() ) then
-		if ( panel:GetDisabled() ) then
-				draw.RoundedBox(d["СheckBox"]["CornerRadius"], 0, 0, w, h, d["СheckBox"]["DisabledActive"])
-		elseif ( panel.Hovered ) then
-			draw.RoundedBox(d["СheckBox"]["CornerRadius"], 0, 0, w, h, d["СheckBox"]["Hovered"])
-		else
-			draw.RoundedBox(d["СheckBox"]["CornerRadius"], 0, 0, w, h, d["СheckBox"]["Active"])
+	if panel:GetChecked() then
+
+		Col = d["СheckBox"]["Active"]
+		ColBord = d["СheckBox"]["BorderActive"]
+
+		if panel:GetDisabled() then
+			Col = d["СheckBox"]["DisabledActive"]
+			ColBord = d["СheckBox"]["BorderDisabledActive"]
 		end
+
+		if ( panel.Hovered ) then
+			Col = d["СheckBox"]["Hovered"]
+			ColBord = d["СheckBox"]["BorderHovered"]
+		end
+
 	else
-		if ( panel:GetDisabled() ) then
-			draw.RoundedBox(d["СheckBox"]["CornerRadius"], 0, 0, w, h, d["СheckBox"]["Disabled"])
-		elseif ( panel.Hovered ) then
-			draw.RoundedBox(d["СheckBox"]["CornerRadius"], 0, 0, w, h, d["СheckBox"]["Hovered"])
-		else
-			draw.RoundedBox(d["СheckBox"]["CornerRadius"], 0, 0, w, h, d["СheckBox"]["NotActive"])
+
+		Col = d["СheckBox"]["NotActive"]
+		ColBord = d["СheckBox"]["BorderNotActive"]
+
+		if panel:GetDisabled() then
+			Col = d["СheckBox"]["Disabled"]
+			ColBord = d["СheckBox"]["BorderDisabled"]
 		end
-	end	
+
+		if panel.Hovered then
+			Col = d["СheckBox"]["Hovered"]
+			ColBord = d["СheckBox"]["BorderHovered"]
+		end
+
+	end
+
+	if d["СheckBox"]["BorderSize"] ~= 0 then
+		draw.RoundedBox(d["СheckBox"]["CornerRadius"], 0, 0, w, h, ColBord)
+	end
+
+	draw.RoundedBox(d["СheckBox"]["CornerRadius"], d["СheckBox"]["BorderSize"], d["СheckBox"]["BorderSize"], w -d["СheckBox"]["BorderSize"] *2, h -d["СheckBox"]["BorderSize"] *2, Col)
 
 end
